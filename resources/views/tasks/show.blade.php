72d2cbd94f0af122989405db9070959ce1e99538
Fio Juliansyah <63538861+fiojuliansyah@users.noreply.github.com>
2026-01-06 11:26:22 +0700

    Update show.blade.php

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -3,13 +3,18 @@
 @section('title', 'Detail Tugas')
 
 @section('content')
-    <div class="content">
-        @if (!$taskProgress)
-            @include('tasks.partials.start')
-        @elseif ($taskProgress->status === 'in_progress')
-            @include('tasks.partials.progress')
-        @else
-            @include('tasks.partials.completed')
-        @endif
-    </div>
+<div class="content">
+    @if (!$taskProgress)
+        @include('tasks.partials.start')
+
+    @elseif ($taskProgress->status === 'in_progress' && !request()->has('end'))
+        @include('tasks.partials.progress')
+
+    @elseif ($taskProgress->status === 'in_progress' && request()->has('end'))
+        @include('tasks.partials.progress-end')
+
+    @elseif ($taskProgress->status === 'completed')
+        @include('tasks.partials.completed')
+    @endif
+</div>
 @endsection
